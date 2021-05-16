288186741c9653a1e10173ca302a49f683466a24
mkayili <52419258+mkayili@users.noreply.github.com>
2021-05-16 23:21:04 +0300

    Add files via upload
    NavBar

---
+++ myScript.js
@@ -0,0 +1,9 @@
+/*var width;
+window.onresize = window.onload = function() {
+	width = window.innerWidth;
+	if (width < 768) {
+		document.getElementById('parag').innerHTML = document.getElementById('navBarMini').innerHTML;
+	} else {
+		document.getElementById('parag').innerHTML = document.getElementById('navBarLarge').innerHTML;
+	}
+}*/
