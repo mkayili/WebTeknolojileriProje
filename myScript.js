09c6a1405c82d05f2642ab38431898e2ab38c3d4
mkayili <52419258+mkayili@users.noreply.github.com>
2021-05-23 23:46:47 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -7,3 +7,21 @@ window.onresize = window.onload = function() {
 		document.getElementById('parag').innerHTML = document.getElementById('navBarLarge').innerHTML;
 	}
 }*/
+
+function validateForm() {
+  var adsoyad = document.forms["myForm"]["adsoyad"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var telefon = document.forms["myForm"]["telefon"].value;
+  var mesaj = document.forms["myForm"]["mesaj"].value;
+  var re = /\S+@\S+\.\S+/;
+  
+  if (adsoyad == "" || email == "" || telefon == "" || mesaj == "") {
+	alert("Lütfen tüm alanları doldurunuz!");
+	return false;
+  }
+  
+  if (!re.test(email)){
+	  alert("Hatali email!");
+	  return false;
+  }
+}
